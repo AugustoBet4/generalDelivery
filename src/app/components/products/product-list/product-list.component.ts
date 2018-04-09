42dc99ca16227bd11ccd07fe3dd433324be613b0
AugustoBet4 <augusto.bet4@gmail.com>
2018-04-09 10:31:58 -0400

    mejoras interfaz

--- src/app/components/products/product-list/product-list.component.ts
+++ src/app/components/products/product-list/product-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 // Service
 import { ProductService } from '../../../services/product.service';
@@ -43,6 +44,25 @@ export class ProductListComponent implements OnInit {
     if (confirm('Esta seguro de eliminar?')) {
       this.productService.deleteProduct($key);
       this.toastr.success('Operación Exitosa', 'Producto Eliminado');
+    }
+  }
+
+  onSubmit(productForm: NgForm) {
+    if (productForm.value.$key == null){
+      this.productService.insertProducts(productForm.value);
+      this.toastr.success('Operación Exitosa','Producto Agregado');
+    }
+    else{
+      this.productService.updateProduct(productForm.value);
+      this.toastr.success('Operación Exitosa','Producto Modificado');
+    }
+    this.resetForm(productForm);
+  }
+
+  resetForm(productForm?: NgForm) {
+    if (productForm != null){
+      productForm.reset();
+      this.productService.selectedProduct = new Product();
     }
   }
 }
